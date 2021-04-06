af914e46eeeb6b361481d0fb6a5a6384465429d6
awsmit07 <awsmit@shaw.ca>
2021-04-06 12:09:05 -0600

    Fix the binary files going to git.

--- Prj400.X/build/default/debug/mcc_generated_files/dac1.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/mcc_generated_files/dac1.o:  \
- mcc_generated_files/dac1.c  \
-mcc_generated_files/dac1.h 
--- Prj400.X/build/default/production/mcc_generated_files/dac1.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/mcc_generated_files/dac1.o:  \
- mcc_generated_files/dac1.c  \
-mcc_generated_files/dac1.h 
